daef8cb7d5c047be5ac8f4bb0ae059510e2534a3
THE PROGRAMMER <142110420+Itxxwasi@users.noreply.github.com>
2025-05-15 22:18:23 +0500

    Update dev.js

--- plugins/dev.js
+++ plugins/dev.js
@@ -1,42 +1,41 @@
-/** CODED BY MR WASI
-*/
-
-
 const { cmd } = require('../command');
 const config = require('../config');
 
 cmd({
     pattern: "dev",
-    react: "💻", 
+    react: "💻",
     desc: "Get developer info",
     category: "main",
     filename: __filename
-}, 
+},
 async (conn, mek, m, { from }) => {
     try {
-        const developers = [
+        const developers = config.DEVELOPERS || [
             {
-                name: "MR WASI",
-                number: "+923192173398",
+                name: "DevOne",
+                number: "+12345678901",
                 image: "https://files.catbox.moe/abc123.jpg"
             },
             {
-                name: "DEBY TECH",
-                number: "+123456789",
+                name: "DevTwo",
+                number: "+10987654321",
                 image: "https://files.catbox.moe/xyz456.jpg"
             },
             {
-                name: "STEVY TECH",
-                number: "+12345678789",
+                name: "DevThree",
+                number: "+11223344556",
                 image: "https://files.catbox.moe/def789.jpg"
             }
         ];
 
         for (const dev of developers) {
-            const vcard = `BEGIN:VCARD\nVERSION:3.0\nFN:${dev.name}\n` +
-                          `TEL;type=CELL;type=VOICE;waid=${dev.number.replace('+', '')}:${dev.number}\nEND:VCARD`;
+            const vcard = `BEGIN:VCARD\n` +
+                          `VERSION:3.0\n` +
+                          `FN:${dev.name}\n` +
+                          `TEL;type=CELL;type=VOICE;waid=${dev.number.replace('+', '')}:${dev.number}\n` +
+                          `END:VCARD`;
 
-            // Send contact vCard
+            // Send vCard contact
             await conn.sendMessage(from, {
                 contacts: {
                     displayName: dev.name,
@@ -63,6 +62,6 @@ async (conn, mek, m, { from }) => {
 
     } catch (error) {
         console.error(error);
-        reply(`An error occurred: ${error.message}`);
+        m.reply(`An error occurred: ${error.message}`);
     }
 });
